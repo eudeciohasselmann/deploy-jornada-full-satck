a7e8948af17cd2e7a9ea5de1af2d2c2487aae065
eudeciohasselmann <eudecio@gmail.com>
2025-02-16 14:59:00 -0300

    Ajustes de deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Só vai responder os endpoint '/artists' e '/songs'");
 });
 
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (req, res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(3000, () => {
