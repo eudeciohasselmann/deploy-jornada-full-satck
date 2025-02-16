c4872ea3bf2eb118aa2fb1654295aa29375dd015
eudeciohasselmann <eudecio@gmail.com>
2025-02-16 15:30:19 -0300

    Ajustes de deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import "dotenv/config";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://deploy-jornada-full-satck.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 
